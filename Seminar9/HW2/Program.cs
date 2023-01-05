e66dcbf0ca3cec681dbf841ab0546b82a2705cc5
Mariya <mariya401@list.ru>
2023-01-05 16:40:10 +0300

    Seminar9

---
+++ Seminar9/HW2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int RecurcySum(int M, int N)
+{
+    if (M <= N)
+        return M + RecurcySum(M + 1, N);
+    else
+        return 0;
+}
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}: {RecurcySum(M, N)} ");
---
+++ Seminar9/HW3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"{Akkerman(m, n)} ");
+
+int Akkerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akkerman(n - 1, 1);
+    else
+        return Akkerman(n - 1, Akkerman(n, m - 1));
+}
